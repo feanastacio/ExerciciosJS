83a962c4be7765bbc223c6bb4972efa869d7dbe7
Fernanda Marques <fernada.anastacio@aluno.senai.br>
2025-04-17 09:54:39 -0300

    feat: Realizando exercicios de JavaScript

--- ExerciciosJs/Ex04.js
+++ ExerciciosJs/Ex04.js
@@ -1,10 +1,15 @@
 // Crie uma função que gera uma senha de 10 caracteres com letras, números e símbolos
 // usando Math.random(), charCodeAt() e String.fromCharCode().
-let qntd = 10;
-function combinacao(qntd) {
-    let senhaaleatoria = [];
-    for (let i = 0; i <= qntd; i++) {
-        let num = Math.random();        
-    }
-
+// let qntd = 10;
+function combinacao() {
+    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&";
+    let senhaaleatoria = "";
+        for (let i = 0; i < 10; i++) {
+            const indice = Math.floor(Math.random() * caracteres.length);
+            senhaaleatoria += caracteres.charAt(indice);        
+        }
+    return senhaaleatoria;
 }
+    console.log(combinacao());
+
+    // Não ta funcionando \\Error: Cannot find module 'C:\Users\50776884867\Desktop\ExerciciosJs\Ex04.js'
--- ExerciciosJs/Ex10.js
+++ ExerciciosJs/Ex10.js
@@ -1,2 +1,17 @@
 // Peça uma frase e retorne a palavra que mais se repete. Use split(), reduce() e um
 // objeto como contador.
+
+var prompt = require('prompt-sync')();
+const frase = prompt("Digite uma frase:");
+
+function palavrasqueserepetem (frase) {
+    const palavra = frase.split(" ");
+    const contador = {};
+        for (palavra of palavrasquerepetem) {
+            contador[palavra] = (contador [palavrasquerepetem] || 0) + 1;
+        }
+    return contador;
+}
+
+let repetidas = "";
+return `A palavra que mais se repete é "${repetidas}"`;
